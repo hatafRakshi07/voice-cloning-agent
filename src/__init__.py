0211c7c2f6d04568a6475dd4959700b0aa06d42e
hataf rakshi <hatafrakshi157@gmail.com>
2026-06-25 14:17:12 +0530

    Add package initialization

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""Voice Cloning Agent Package"""
+
+__version__ = "1.0.0"
+__author__ = "Voice Cloning Team"
+__description__ = "An intelligent voice cloning agent for voice synthesis and conversion"
+
+from .agent.voice_agent import VoiceAgent
+
+__all__ = ["VoiceAgent"]
